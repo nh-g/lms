61e4f214bf78b9a5cd03d56d57239aa429711ae2
nh-g <giangngohong@gmail.com>
2021-11-02 21:04:41 +0100

    implement ImageUploader to inline Edit form

--- src/components/teacher/Card.tsx
+++ src/components/teacher/Card.tsx
@@ -3,12 +3,15 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+// Project files
+
 import Placeholder from "assets/brand/holder.png";
 import Delete from "components/shared/Delete";
 import ButtonEdit from "components/shared/ButtonEdit";
 import { updateDocument } from "scripts/fireStore";
 import InputEditable from "./InputEditable";
 import fields from "assets/fields/fields-edit.json";
+import ImageUploader from "components/shared/ImageUploader";
 
 interface iProps {
   item: object;
@@ -20,11 +23,15 @@ export default function Card({ item }: iProps) {
   const history = useHistory();
 
   const [form, setForm] = useState(item);
+  const [courseImageURL, setCourseImageURL] = useState(item.imageURL);
+
 
   async function onSubmit(event) {
     if (window.confirm("Are you sure to update content?")) {
       event.preventDefault();
-      await updateDocument("courses", item.id, { ...item, ...form });
+      const updatedCourse = {...form}
+      updatedCourse.imageURL = courseImageURL;
+      await updateDocument("courses", item.id, { ...item, ...updatedCourse });
       alert("Course successfully updated");
       history.push("/");
     }
@@ -48,18 +55,18 @@ export default function Card({ item }: iProps) {
   return (
     <tr>
       {TitleDescription}
-      <td>
-        <img
-          src={Image}
-          alt="user generated content"
-          className="custom-file-chooser"
+      <td className="custom-file-chooser">
+        <ImageUploader
+          imageURL={courseImageURL}
+          setImageURL={setCourseImageURL}
+          title={form.title}
         />
       </td>
       <td className="admin-options">
         <Delete path="courses" dataSelected={item} />
       </td>
       <td className="admin-options">
-        <ButtonEdit handleClick={onSubmit}/>
+        <ButtonEdit handleClick={onSubmit} />
       </td>
     </tr>
   );

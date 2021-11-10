3c1782f34ea8c3eb46d5cdcf903f6720cde69287
nh-g <giangngohong@gmail.com>
2021-11-11 00:22:00 +0100

    add empty check

--- src/components/shared/MappingList.jsx
+++ src/components/shared/MappingList.jsx
@@ -1,4 +1,8 @@
 export default function MappingList({ Component, mapData }) {
   const List = mapData.map((item) => <Component key={item.id} item={item} />);
-  return <>{List}</>;
+  return <>
+    {List.length > 0? List : 
+    <h3>Currently no content to show. More are coming...</h3>
+    }
+  </>;
 }

db21111f872acda74dce68c92b080a65bdb849f3
nh-g <giangngohong@gmail.com>
2021-11-11 00:16:20 +0100

    fix bug: need to reload page to see added course. Fixed by use custom hook to fetch updated global state

--- src/components/shared/BoxError.jsx
+++ src/components/shared/BoxError.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function BoxError() {
   return (
     <div className="load-error">
--- src/components/shared/CoursesList.jsx
+++ src/components/shared/CoursesList.jsx
@@ -1,36 +1,23 @@
 // Project files
-import useFetch from "hooks/useFetch";
 import { useCourses } from "state/CoursesProvider";
 import { useUser } from "state/UserProvider";
-
 import CardInTeacherPage from "../teacher/CardCourse";
 import CardInStudentPage from "../student/CardCourse";
-import Spinner from "./Spinner";
-import BoxError from "./BoxError";
 import MappingList from "./MappingList";
 
 export default function CoursesList() {
-  const { dispatch } = useCourses();
-  const courses = useFetch("courses", dispatch);
-
-  console.log("CourseList", courses);
-
+  // Global state
+  const { courses } = useCourses();
   const { user } = useUser();
+  
   const isTeacher = user.role === "teacher";
 
   return (
     <>
-      {courses.loading === true && <Spinner />}
-      {courses.error !== null && <BoxError />}
-
-      {(!courses.loading && courses.error) === null && (
-        <>
-          {isTeacher ? (
-            <MappingList Component={CardInTeacherPage} mapData={courses.data} />
-          ) : (
-            <MappingList Component={CardInStudentPage} mapData={courses.data} />
-          )}
-        </>
+      {isTeacher ? (
+        <MappingList Component={CardInTeacherPage} mapData={courses} />
+      ) : (
+        <MappingList Component={CardInStudentPage} mapData={courses} />
       )}
     </>
   );
